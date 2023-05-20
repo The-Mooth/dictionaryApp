8c1b784ea92acf06e83c68154e7a511cd3077df5
Jacob-Eason <easonj2@wwu.edu>
2023-05-20 15:18:10 -0700

    added svgs

---
+++ src/components/svgs/arrow.js
@@ -0,0 +1,8 @@
+import * as React from "react"
+import Svg, { Path } from "react-native-svg"
+const arrow = (props) => (
+  <Svg xmlns="http://www.w3.org/2000/svg" width={14} height={8} {...props}>
+    <Path fill="none" stroke="#A445ED" strokeWidth={1.5} d="m1 1 6 6 6-6" />
+  </Svg>
+)
+export default arrow
